89b6ec2496e69e48819b18c23a4175699c0febe0
AgniechaP <agnpior@gmail.com>
2023-03-12 01:16:51 +0100

    feedback linearization controller

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -12,4 +12,19 @@ class FeedbackLinearizationController(Controller):
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+
+        # q - wyjście z obiektu
+        # qr - wartość zadana
+        q1, q2, q1_dot, q2_dot = x
+
+        q_t = x[:2]
+        q_t_dot = x[2:]
+
+        K_d = [[250, 0], [0, 30]]
+        K_p = [[250, 0], [0, 30]]
+
+        v = q_r_ddot + K_d @ (q_r_dot - q_t_dot) + K_p @ (q_r - q_t)
+
+        tau = self.model.M(x) @ v[:, np.newaxis] + self.model.C(x) @ q_r_dot[:, np.newaxis]
+
+        return tau
